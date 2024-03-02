e8cf166bf3bba92b8850b4eb7d48fd18d61a3477
Donald Jones <djcommits@gmail.com>
2024-03-02 10:20:30 -0600

    mvp done

--- STLSPSpotifyBot.py
+++ STLSPSpotifyBot.py
@@ -1,5 +1,44 @@
-from spotipy import *
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import creds
+import discord
 
-def SpotifyAuthentication():
-    sp = spotipy.Spotyfy(auth_manager=SpotifyOAuth(scope="user-library-read"))
+
+        
+
+def SpotifyAuthentication(id, secret):
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id= id, client_secret=secret, redirect_uri='http://127.0.0.0:9090', scope="playlist-modify-public playlist-modify-private"))
     return sp
+
+
+def CreatePlaylist(sp, title):
+    user_id = sp.me()['id']
+    return sp.user_playlist_create(user_id, title)['id']
+    
+
+def AddToPlaylist(link):
+    sp.playlist_add_items(playlist_ID, [link])
+    
+    
+    
+sp = SpotifyAuthentication(creds.SP_CLIENT_ID, creds.SP_CLIENT_SECRET)
+playlist_ID = r'0L8hjTMfDnWNzW95AhAmt7'
+intents = discord.Intents.default()
+intents.message_content = True
+discordClient = discord.Client(intents=intents)
+
+@discordClient.event
+async def on_ready(self):
+    print(f'Logged on as {self.user}!')
+        
+@discordClient.event
+async def on_message(message):
+    if 'spotify.com' in message.content:
+        print(f'Detected spotify link from: {message.author} ')
+        link = message.content
+        AddToPlaylist(link)
+
+
+
+discordClient.run(creds.DC_CLIENT_TOKEN)
+
