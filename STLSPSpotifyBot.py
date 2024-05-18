7b2837d6b14d77762fe6fba844a401438be6e720
Donald Jones <djcommits@gmail.com>
2024-05-18 01:34:00 -0500

    Began support of automatic playl,ist creation

--- STLSPSpotifyBot.py
+++ STLSPSpotifyBot.py
@@ -1,3 +1,4 @@
+from datetime import date
 from math import e
 from tkinter.messagebox import CANCEL
 import spotipy
@@ -6,20 +7,37 @@ import creds
 import discord
 
 
-        
+    
 
 def SpotifyAuthentication(id, secret):
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id= id, client_secret=secret, redirect_uri='http://127.0.0.0:9090', scope="playlist-modify-public playlist-modify-private"))
     return sp
 
+# variables  
+sp = SpotifyAuthentication(creds.SP_CLIENT_ID, creds.SP_CLIENT_SECRET)
+playlist_ID = ""
+message_check = ':banger:'
+intents = discord.Intents.default()
+intents.message_content = True
+discordClient = discord.Client(intents=intents)
+messages = {}
+month = date.month
+year = date.year
 
 def CreatePlaylist(sp, title):
-    user_id = sp.me()['id']
-    return sp.user_playlist_create(user_id, title)['id']
+    current_playlists = sp.user_playlists(sp.me()['id'], fields="name")
+    for i in current_playlists:
+        if i["name"] == f"STLSP {month} {year} Playlist":
+            return i['id']
+        else:
+            user_id = sp.me()['id']
+            return sp.user_playlist_create(user_id, title)['id']
+        
+    
     
 
 def AddToPlaylist(track):
-    # Check for song, if not in playlist, then add to playlist
+    # Check that playlist exists, if so, check if duplicate, if not, add to playlist
     
     if(CheckIfExistsInPlaylist(track)):
         sp.playlist_add_items(playlist_ID,  [track['uri']])
@@ -32,26 +50,6 @@ def CheckIfExistsInPlaylist(track):
             return False
     return True
     
-    
-sp = SpotifyAuthentication(creds.SP_CLIENT_ID, creds.SP_CLIENT_SECRET)
-playlist_ID = r'0L8hjTMfDnWNzW95AhAmt7'
-message_check = ':banger:'
-intents = discord.Intents.default()
-intents.message_content = True
-discordClient = discord.Client(intents=intents)
-messages = {}
-playlist_titles = {"January" : "STLSP January 2025 Playlist",
-                   "February" : "STLSP February 2025 Playlist",
-                   "March" : "STLSP March 2025 Playlist",
-                   "April" : "STLSP April 2025 Playlist",
-                   "May" : "STLSP May 2024 Playlist",
-                   "June" : "STLSP June 2024 Playlist",
-                   "July" : "STLSP July 2024 Playlist",
-                   "August" : "STLSP August 2024 Playlist",
-                   "September" : "STLSP September 2024 Playlist",
-                   "October" : "STLSP October 2024 Playlist",
-                   "November" : "STLSP November 2024 Playlist",
-                   "December" : "STLSP December 2024 Playlist",}
 
 
 @discordClient.event
@@ -60,6 +58,9 @@ async def on_ready(self):
         
 @discordClient.event
 async def on_message(message):
+    global playlist_ID
+    if playlist_ID == "":
+        playlist_ID = CreatePlaylist(sp, f"STLSP {month} {year} Playlist" )
     if 'spotify.com' in message.content:
         link = []
         for word in message.content.split():
@@ -84,7 +85,6 @@ async def on_reaction_add(reaction, user):
 #TODO: handle removal of banger emoji, archival and creation of playlist
         
 
-
-
 discordClient.run(creds.DC_CLIENT_TOKEN)
+
 
