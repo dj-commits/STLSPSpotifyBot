d422623716333b8a399e9983df860f000a939ec8
djcommits <djcommits@gmail.com>
2024-05-31 17:42:00 -0500

    Continuing to optimize and adjust

--- STLSPSpotifyBot.py
+++ STLSPSpotifyBot.py
@@ -1,4 +1,6 @@
+import asyncio
 import datetime
+from platform import java_ver
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import creds
@@ -14,6 +16,7 @@ def SpotifyAuthentication(id, secret):
 # variables  
 sp = SpotifyAuthentication(creds.SP_CLIENT_ID, creds.SP_CLIENT_SECRET)
 playlist_ID = ""
+playlist_link = ""
 message_check = ':banger:'
 intents = discord.Intents.default()
 intents.message_content = True
@@ -22,7 +25,7 @@ messages = {}
 today = datetime.date.today()
 month = today.strftime("%B")
 year = today.strftime("%Y")
-me = sp.me()
+me = creds.SP_USER_ID
 message_counter = 0
 
 
@@ -38,12 +41,19 @@ def UpdateCurrentPlaylist(this_sp):
 
 
 def CreatePlaylist(sp, title):
+    global playlist_link
+    global me
     current_playlists = sp.user_playlists(sp.me()['id'])
     for i in current_playlists["items"]:
         if f"STLSP {month} {year} Playlist" in i["name"]:
+            playlist_link = i['external_urls']['spotify']
+            print("I just created a playlist")
             return i['id']
     user_id = sp.me()['id']
-    return sp.user_playlist_create(user_id, title)['id']   
+    new_playlist = sp.user_playlist_create(user_id, title)
+    playlist_link = new_playlist['external_urls']['spotify']
+    print("I just created a playlist")
+    return new_playlist['id']
         
     
     
@@ -51,6 +61,7 @@ def CreatePlaylist(sp, title):
 def AddToPlaylist(track):    
     if(CheckIfExistsInPlaylist(track)):
         sp.playlist_add_items(playlist_ID,  [track['uri']])
+        print("I just added a song")
         
 def CheckIfExistsInPlaylist(track):
     items = sp.playlist_items(playlist_ID)
@@ -59,12 +70,6 @@ def CheckIfExistsInPlaylist(track):
         if i['track']['uri'] == track['uri']:
             return False
     return True
-
-def Promote():
-    global sp
-    playlist_link = sp.playlist(playlist_ID)["external_urls"]["spotify"]
-    promotion = f"STLSP's {month} {year} Playlist is available here: \n {playlist_link} \n"
-    return promotion
     
 
 
@@ -76,12 +81,16 @@ async def on_ready(self):
 async def on_message(message):
     global playlist_ID
     global message_counter
+    global playlist_link
     if playlist_ID == "":
         playlist_ID = CreatePlaylist(sp, f"STLSP {month} {year} Playlist" )
-    if message_counter >= 2:
-        await message.channel.send(Promote())
+    if message_counter >= 50:
         message_counter = 0
-    if 'spotify.com' in message.content:
+        async with asyncio.timeout(10):
+            await message.channel.send(f"STLSP's {month} {year} Playlist is available here: \n {playlist_link} \n")
+    if f"{playlist_link}" in message.content:
+        print("This is how I stop looping over myself.")
+    elif 'spotify.com' in message.content:
         link = []
         for word in message.content.split():
             if 'spotify.com' in word:
@@ -96,10 +105,11 @@ async def on_message(message):
 @discordClient.event
 async def on_reaction_add(reaction, user):
     global sp
+    global playlist_
     UpdateCurrentPlaylist(sp)
     if message_check in str(reaction):
         for i in messages:
-            if "STLSP's {month} {year} Playlist is available here: \n {playlist_link} \n" in reaction.message.content:
+            if f"STLSP's {month} {year} Playlist is available here: \n {playlist_link} \n" in reaction.message.content:
                 break
             if i == str(reaction.message.id):
                 AddToPlaylist(messages[i])
@@ -107,7 +117,7 @@ async def on_reaction_add(reaction, user):
 
 
 
-#TODO: handle removal of banger emoji, archival and creation of playlist
+#TODO: handle removal of banger emoji
         
 
 discordClient.run(creds.DC_CLIENT_TOKEN)
